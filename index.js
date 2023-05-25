edd26a412c77f0fd1feec6e4d8afa3f9c22d80d8
Maryna Zhelobetska <marynazhelobetska@gmail.com>
2023-05-25 20:58:18 +0300

    Escape

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-console.log(352);
-alert(24566);
-console.log(true);
-console.log('This is text');
+
+let box1 = null;
+console.log(box1);
+
+let box2 = 5;
+console.log(box2);
+
+let box3 = 2;
+console.log(box3);
+
+let box4 = box2 + box3;
+console.log(box4);
+
+let box5 = box3 - box2;
+console.log(box5);
+
+let box6 = box1 + box2;
+console.log(box6);
+
+let box7 = 10 + undefined;
+console.log(box7);
+
+const sum = (2 + 2) * 6;
+console.log(sum);
+
+const hello = 'Маша сказала "Привет"';
+console.log(hello);
+
+const hello2 = "Маша сказала \"Привет\"";
+console.log(hello2);
