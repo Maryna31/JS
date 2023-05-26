d8c563095c23a0f1e68b54e1b15b5ef690f58340
Maryna Zhelobetska <marynazhelobetska@gmail.com>
2023-05-26 20:29:43 +0300

    Operator if

---
+++ index1.js
@@ -0,0 +1,10 @@
+let r = 5;
+if ( r > 12) {
+    console.log('Більше 12');
+} else if ( r < 5) {
+        console.log('Менша за 5');
+    } else {
+        console.log('в діапазоні між 5 і 12');
+    }
+
+
