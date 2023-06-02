13121e91b4b6c40e2274a74385cf223f4390f3fe
Maryna Zhelobetska <marynazhelobetska@gmail.com>
2023-06-02 20:44:03 +0300

    greetingUser

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 
-let box1 = null;
-console.log(box1);
 
-let box2 = 5;
-console.log(box2);
 
-let box3 = 2;
-console.log(box3);
+const user= {
+    name: 'John',
+    surname: 'Doe',
+    age: '35',
+}
 
-let box4 = box2 + box3;
-console.log(box4);
 
-let box5 = box3 - box2;
-console.log(box5);
-
-let box6 = box1 + box2;
-console.log(box6);
-
-let box7 = 10 + undefined;
-console.log(box7);
-
-const sum = (2 + 2) * 6;
-console.log(sum);
-
-const hello = 'Маша сказала "Привет"';
-console.log(hello);
-
-const hello2 = "Маша сказала \"Привет\"";
-console.log(hello2);
+function greetingUser(user) {
+    return `Hey ${user.name} ${user.surname}. You will be ${user.age + 1} net year`
+}
